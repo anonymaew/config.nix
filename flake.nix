4c8a61283053b06df52cfdc38d1c065aa0875a63
Napat Srichan <me@napatsc.com>
2025-12-08 09:50:07 +0700

    add homelab, proxy server

--- flake.nix
+++ flake.nix
@@ -27,14 +27,25 @@
     brew-nix = {
       url = "github:BatteredBunny/brew-nix";
       inputs.brew-api.follows = "brew-api";
+      inputs.nix-darwin.follows = "nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    # for MacOS apps fix
+    mac-app-util = {
+      url = "github:hraban/mac-app-util";
+    };
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
 
   outputs = {
+    self,
+    nixpkgs,
     nix-darwin,
     home-manager,
     brew-nix,
     nixvim,
+    mac-app-util,
+    deploy-rs,
     ...
   }: let
     vars = {
@@ -46,19 +57,58 @@
         system = "aarch64-darwin";
         modules = [
           ./default.nix
+          ./system.nix
           brew-nix.darwinModules.default
           home-manager.darwinModules.home-manager
+          mac-app-util.darwinModules.default
           {
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
-              # backupFileExtension = "backup";
-              users."${vars.name}" = import ./home.nix;
-              sharedModules = [nixvim.homeManagerModules.nixvim];
+              users."${vars.name}".imports = [
+                ./home.nix
+                (./. + "/users/${vars.name}")
+                nixvim.homeModules.nixvim
+                mac-app-util.homeManagerModules.default
+              ];
             };
           }
         ];
         specialArgs = {inherit vars;};
+      };
+    };
+    nixosConfigurations.homelab = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [./homelab/configuration.nix];
+      specialArgs = {inherit vars;};
+    };
+    deploy.nodes.homelab = {
+      hostname = "homelab";
+      interactiveSudo = true;
+      remoteBuild = true;
+      profiles.system = {
+        sshUser = "napatsc";
+        user = "root";
+        path =
+          deploy-rs.lib.x86_64-linux.activate.nixos
+          self.nixosConfigurations.homelab;
+      };
+    };
+    nixosConfigurations.linode = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [./linode/configuration.nix];
+      specialArgs = {inherit vars;};
+    };
+    deploy.nodes.linode = {
+      hostname = "linode";
+      interactiveSudo = true;
+      remoteBuild = true;
+      profiles.system = {
+        sshUser = "napatsc";
+        user = "root";
+        path =
+          deploy-rs.lib.x86_64-linux.activate.nixos
+          self.nixosConfigurations.linode;
       };
     };
   };
---
+++ linode/configuration.nix
@@ -0,0 +1,183 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  pkgs,
+  vars,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Bootloader.
+  boot.loader = {
+    grub.device = "nodev";
+    timeout = 10;
+  };
+
+  networking.hostName = "linode"; # Define your hostname.
+
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+  };
+  networking.enableIPv6 = true;
+  # systemd.user.services = {
+  #   "podman.socket".enable = true;
+  #   "podman-restart.service".enable = true;
+  # ExecStart=/nix/store/a6afd7k1i8036rsdrf1dnj7pylg27pzb-podman-5.6.1/bin/podman $LOGGING start --all --filter restart-policy=always
+  # ExecStop=/nix/store/a6afd7k1i8036rsdrf1dnj7pylg27pzb-podman-5.6.1/bin/podman  $LOGGING stop  --all --filter restart-policy=always
+
+  # };
+
+  networking.firewall = {
+    enable = true;
+    allowedUDPPorts = [51820];
+  };
+  networking.wg-quick.interfaces.server = {
+    autostart = true;
+    configFile = "/home/napatsc/server.conf";
+  };
+
+  # make predictable network; eth0 available
+  # networking.usePredictableInterfaceNames = false;
+  # networking.useDHCP = false;
+  # networking.interfaces.eth0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  # networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Bangkok";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # # Enable the X11 windowing system.
+  # services.xserver = {
+  #   enable = false;
+  #   # Enable the GNOME Desktop Environment.
+  #   displayManager.gdm.enable = true;
+  #   desktopManager.gnome.enable = true;
+  #   # Configure keymap in X11
+  #   xkb = {
+  #     layout = "us";
+  #     variant = "";
+  #   };
+  # };
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  # services.pulseaudio.enable = false;
+  # security.rtkit.enable = true;
+  # services.pipewire = {
+  #   enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  #   pulse.enable = true;
+  #   # If you want to use JACK applications, uncomment this
+  #   #jack.enable = true;
+  #
+  #   # use the example session manager (no others are packaged yet so this is enabled by default,
+  #   # no need to redefine it in your config for now)
+  #   #media-session.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users = {
+    "${vars.name}" = {
+      isNormalUser = true;
+      extraGroups = ["wheel"];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDe/pgCTH3B4AzKAVMcA2l42jJq2K4tiObkLNsvbrJZG napatsc@macair"
+      ];
+    };
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    btop
+    inetutils
+    neovim
+    uutils-coreutils-noprefix
+    wget
+    wireguard-tools
+  ];
+
+  # virtualisation = {
+  #   containers = {
+  #     enable = true;
+  #     registries.search = ["docker.io"];
+  #   };
+  #   oci-containers.backend = "podman";
+  #   podman = {
+  #     enable = true;
+  #     extraPackages = with pkgs; [podman-compose];
+  #     autoPrune.enable = true;
+  #     dockerSocket.enable = true;
+  #     defaultNetwork.settings.dns_enabled = true;
+  #   };
+  # };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    ports = [2222];
+    settings = {
+      PasswordAuthentication = false;
+    };
+  };
+
+  # services.tailscale.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+}
