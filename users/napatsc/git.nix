be2dadf802a75c1a3ad6f745afb4660c6bc2e6cd
Napat Srichan <me@napatsc.com>
2026-06-20 17:21:03 +0700

    home: add custom git pager

--- users/napatsc/git.nix
+++ users/napatsc/git.nix
@@ -1,9 +1,20 @@
 {...}: {
   programs.git = {
     enable = true;
-    settings.user = {
-      name = "Napat Srichan";
-      email = "me@napatsc.com";
+    settings = {
+      user = {
+        name = "Napat Srichan";
+        email = "me@napatsc.com";
+      };
+
+      # git syntax highlight via delta
+      core.pager = "delta";
+      interactive.diffFilter = "delta --color-only";
+      delta = {
+        navigate = true; # use n and N to move between diff sections
+        dark = true; # or light = true, or omit for auto-detection
+      };
+      merge.conflictStyle = "zdiff3";
     };
     signing = {
       signByDefault = true;
